d565788f88efc5bd414c7e01c5701a433b48b08c
vednav9 <vedantnavthale222@nhitm.ac.in>
2026-03-08 19:30:43 +0530

    feat(backend): initialize passport for authentication

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import passport from "passport";
 
 import { env } from "./src/config/env.js";
 import logger from "./src/utils/logger.js";
@@ -7,12 +8,14 @@ import errorHandler from "./src/middlewares/errorHandler.js";
 import requestLogger from "./src/middlewares/requestLogger.js";
 import routes from "./src/routes/index.js";
 import authRoutes from "./src/routes/authRoutes.js";
+import "./src/config/googleStrategy.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
+app.use(passport.initialize());
 
 app.get("/", (req, res) => {
     res.json({ message: "API is running" });
