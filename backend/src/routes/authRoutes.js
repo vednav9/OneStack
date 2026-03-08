77ce451ae5c7736e5261ebf40554912ece7a4170
vednav9 <vedantnavthale222@nhitm.ac.in>
2026-03-08 19:30:19 +0530

    feat(backend): add Google authentication routes and callback handling

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,27 @@
 import express from "express";
+import passport from "passport";
 
-import {register,login} from "../controllers/authController.js";
+import { register, login } from "../controllers/authController.js";
+import { generateAccessToken } from "../utils/jwt.js";
 
-const router=express.Router();
+const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get(
+    "/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"],
+    }),
+);
+
+router.get(
+    "/google/callback",
+    passport.authenticate("google", { session: false }),
+    (req, res) => {
+        const token = generateAccessToken(req.user.id);
+        res.redirect(`http://localhost:5173/auth?token=${token}`);
+    },
+);
 
 export default router;
