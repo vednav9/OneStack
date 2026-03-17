0176104aad3f70950fea076f28920b836c6b5224
vednav9 <vedantnavthale222@nhitm.ac.in>
2026-03-18 00:05:38 +0530

    feat(backend): add routes for saving, liking, and reading blogs with authentication

--- backend/src/routes/blogRoutes.js
+++ backend/src/routes/blogRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getBlogs, getBlog } from "../controllers/blogController.js";
+import { getBlogs, getBlog, saveBlogController, likeBlogController, readBlog } from "../controllers/blogController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getBlogs);
 router.get("/:id", getBlog);
+router.get("/:id/save", authMiddleware, saveBlogController);
+router.post("/:id/like", authMiddleware, likeBlogController);
+router.post("/:id/read", authMiddleware, readBlog);
 
 export default router;
