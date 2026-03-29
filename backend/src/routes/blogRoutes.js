ff8478dead74ba1a0d0407f1796ab9272155382f
vednav9 <vedantnavthale222@nhitm.ac.in>
2026-03-29 23:10:38 +0530

    feat(blogRoutes): add unsave and unlike blog functionality to routes

--- backend/src/routes/blogRoutes.js
+++ backend/src/routes/blogRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getBlogs, getBlog, saveBlogController, likeBlogController, readBlog } from "../controllers/blogController.js";
+import {
+    getBlogs, getBlog,
+    saveBlogController, unsaveBlogController,
+    likeBlogController, unlikeBlogController,
+    readBlog
+} from "../controllers/blogController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -7,7 +12,9 @@ const router = express.Router();
 router.get("/", getBlogs);
 router.get("/:id", getBlog);
 router.post("/:id/save", authMiddleware, saveBlogController);
+router.delete("/:id/save", authMiddleware, unsaveBlogController);
 router.post("/:id/like", authMiddleware, likeBlogController);
+router.delete("/:id/like", authMiddleware, unlikeBlogController);
 router.post("/:id/read", authMiddleware, readBlog);
 
 export default router;
