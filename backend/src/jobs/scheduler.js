9f2ef3773e79a60e53f080a6e86df77dcd1dd133
vednav9 <vedantnavthale222@nhitm.ac.in>
2026-04-07 07:33:23 +0530

    fix(scheduler): change cron schedule to run crawler every minute

--- backend/src/jobs/scheduler.js
+++ backend/src/jobs/scheduler.js
@@ -2,6 +2,6 @@ import cron from "node-cron";
 import { runCrawler } from "./rssCrawler.js";
 
 // Run once per hour at minute 0.
-cron.schedule("0 * * * *", () => {
+cron.schedule("* * * * *", () => {
     runCrawler();
 });
